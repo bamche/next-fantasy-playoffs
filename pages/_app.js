43881e8d99e4343d171265e8b117e7221568b96c
bamche88 <76884966+bamche88@users.noreply.github.com>
2021-12-12 00:39:07 -0800

    boilerplate for app and index

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,56 @@
-import '../styles/globals.css'
+import React, { Fragment, useEffect } from 'react';
+// import { Provider } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import Layout from '../components/layout';
+// import PropTypes from 'prop-types';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Stying imports
+// import { ThemeProvider } from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+
+
+function MyApp(props) {
+  const router = useRouter();
+  const { Component, pageProps } = props;
+  const pathsToExcludeHeaderFooter = ['/signin'];
+  const showHeaderFooter = !pathsToExcludeHeaderFooter.includes(router.pathname);
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>NFL Fantasy Playoffs</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      {/* <ThemeProvider theme={theme}> */}
+        {/* <Provider session={pageProps.session}> */}
+            {showHeaderFooter && (
+              <Layout>
+                
+                  {/* <CssBaseline /> */}
+                  <Component {...pageProps} />
+                
+              </Layout>
+            )}
+            {!showHeaderFooter && <Component {...pageProps} />}
+          {/* </Provider> */}
+        
+      {/* </ThemeProvider> */}
+    </Fragment>
+  );
 }
 
-export default MyApp
+// MyApp.propTypes = {
+//   Component: PropTypes.elementType.isRequired,
+//   pageProps: PropTypes.object.isRequired,
+// };
+
+export default MyApp;
