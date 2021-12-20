a1a0cdb8fffd2ce4261ca509794123a86f107c71
bamche88 <76884966+bamche88@users.noreply.github.com>
2021-12-20 02:14:14 -0800

    player stats api migrated

---
+++ pages/api/player-stats.js
@@ -0,0 +1,37 @@
+import db from '../../lib/playerDataModels'
+
+export default async function playerStats(req, res) {
+  const { email } = req.query;
+  try{
+    //intial call to retieve player ID, then put into array for ordering since results return unordered
+    const playerListQueryString = `SELECT (qb, rb1, rb2, wr1, wr2, te, flex1, flex2, flex3, flex4, k, dst) FROM public.user_list WHERE email= '${email}';`;
+    const SQLplayerIDList = await db.query(playerListQueryString);
+    
+    const rawPlayerIDList = SQLplayerIDList.rows[0].row;
+    const playerIDList = rawPlayerIDList.replace('(' , '').replace(')', '').split(',');
+
+    const playerStats = []
+    
+    const statsQueryString = `SELECT * FROM public.player_list WHERE player_id IN ${rawPlayerIDList}; `
+    const individualTeamStats =  (await db.query(statsQueryString)).rows;
+    
+    //reorder results
+    playerIDList.forEach( id => {
+      for(element of individualTeamStats){
+        if(element.player_id == id) playerStats.push(element);
+      };
+    });
+
+    //pull defense stats
+    const defQueryString = `SELECT * FROM public.def_list WHERE def_id= ${playerIDList[playerIDList.length-1]}; `
+    const defStats =  (await db.query(defQueryString)).rows[0];
+        
+    res.status(200).send({ playerStats, defStats })
+
+  } catch(e) {
+    console.log(`player-stats api error (fetching all players):  ${e}`);
+    res.status(500).send(e);
+  }
+  
+    
+}
