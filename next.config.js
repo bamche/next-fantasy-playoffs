eb805255a7a5cc5e4bb0078a3a695eb2eb5e7678
bamche88 <76884966+bamche88@users.noreply.github.com>
2022-01-13 22:18:33 -0800

    removed cors

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
 // next.config.js
 module.exports = {
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "/api/auth/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-        ],
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
       },
-    ];
+    ]
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/auth/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST" },
+  //         { key: "Access-Control-Allow-Headers", value: "*" },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
