d9b867eb5eb0480550da4043f19f5638024d571c
bamche88 <76884966+bamche88@users.noreply.github.com>
2022-01-16 14:33:58 -0800

    workaround for .replaceAll

--- pages/api/detailed-stats.js
+++ pages/api/detailed-stats.js
@@ -8,27 +8,13 @@ export default async function detailedPlayerStats(req, res) {
     const playerListQueryString = `SELECT (qb, rb1, rb2, wr1, wr2, te, flex1, flex2, flex3, flex4) FROM public.user_list;`;
     const SQLplayerIDList = await db.query(playerListQueryString);
     
-    let idString = ''
+    let idString = '(';
     
-    //cache to avoid adding duplicate player ids since there will be repeats from multiple teams
-    // const duplicateCache = {};
-    // SQLplayerIDList.rows.forEach( row => {
-    //   const arrayOfIDs = row.row.replace('(' , '').replace(')', '').split(',')
-    //   arrayOfIDs.forEach( id => {
-    //     if(duplicateCache[id]) return;
-    //     duplicateCache[id] = true;
-    //     playerIDList.push(id)
-    //   })
-    // })
-
     //process each row to make a list of players to check against
-    SQLplayerIDList.rows.forEach( row => idString += row.row )
-    console.log(SQLplayerIDList)
-    console.log(idString, typeof idString);
-    const rawPlayerIDList = idString.replaceAll(')(' , ',')
-    const statsQueryString = `SELECT * FROM public.player_list WHERE player_id IN ${rawPlayerIDList}; `
+    SQLplayerIDList.rows.forEach( row => idString += `${row.row.replace('(' , '').replace(')', '')}, ` );
+    const rawPlayerIDList = (idString.slice(0, -2));
+    const statsQueryString = `SELECT * FROM public.player_list WHERE player_id IN ${rawPlayerIDList}); `
     const offensePlayerStats =  (await db.query(statsQueryString)).rows;
-    console.log(offensePlayerStats);
         
     res.status(200).send({ offensePlayerStats })
 
