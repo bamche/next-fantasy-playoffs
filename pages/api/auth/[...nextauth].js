bc9808bd412071486726c7e872b4e86d21275a60
Blake <blake.myrick@gmail.com>
2023-01-08 17:47:45 -0800

    Add google oauth boilerplate

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,6 @@
-// import NextAuth from 'next-auth';
-// import EmailProvider from 'next-auth/providers/email';
-// import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
-// import clientPromise from '../../../lib/mongodb'
-
-// export default NextAuth({
-//   providers: [
-//     // Passwordless / email sign in
-//     EmailProvider({
-//       server: process.env.EMAIL_SERVER,
-//       // {
-//       //   host: process.env.SENDGRID_SERVER,
-//       //   port: process.env.SENDGRID_PORT_TLS,
-//       //   auth: {
-//       //     user: process.env.SENDGRID_USERNAME,
-//       //     pass: process.env.SENDGRID_PASSWORD,
-//       //   },
-//       // },
-//       from: `Blake Myrick <${process.env.SENDGRID_FROM}>`,
-//     }),
-//   ],
-//   secret: 'lTkFKhGAM8lnnIFMK0IsLsMCDMSaL61139+HrkqDQYs=',
-//   adapter: MongoDBAdapter(clientPromise),
-// });
 
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
-import { FirebaseAdapter } from '@next-auth/firebase-adapter';
+import GoogleProvider from "next-auth/providers/google";
 
 import firebase from '../../../firebase/clientApp';
 
@@ -33,18 +8,10 @@ const firestore = firebase.firestore();
 
 export default NextAuth({
   providers: [
-    // Passwordless / email sign in
-    Providers.Email({
-      server: {
-        host: process.env.SENDGRID_SERVER,
-        port: process.env.SENDGRID_PORT_TLS,
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
-        },
-      },
-      from: `Blake Myrick <${process.env.SENDGRID_FROM}>`,
-    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
   ],
   adapter: FirebaseAdapter(firestore),
 });
