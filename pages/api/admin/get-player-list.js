742ff0e6aa83c33e0e98c474f93dca6506fcb00a
Blake <blake.myrick@gmail.com>
2023-01-16 22:28:22 -0800

    Add routes for updating DB and calculate points

---
+++ pages/api/admin/get-player-list.js
@@ -0,0 +1,80 @@
+const axios = require('axios');
+const format = require('pg-format');
+
+export default async function getPlayerList(req, res) {
+    const token = Buffer.from(`${process.env.API_KEY}:${process.env.PASSWORD}`, 'utf8').toString('base64')
+
+    const headers = {
+        'Authorization': `Basic ${token}`
+    }
+    const baseURL = process.env.PLAYERS_URL;
+    const season = '2022-2023-regular';
+    const team = 'buf,tb,kc,cin,sf,phi,dal,sea,nyg,lac,min,mia,jax,bal';
+    const position = 'qb,rb,wr,te,k';
+    
+    const url = baseURL + `?season=${season}&team=${team}&position=${position}`;
+
+    const parameters = {
+        url,
+        method: 'get',
+        headers,
+    };
+
+    try{
+        const playerList = await axios(parameters);
+        const [playerData, teamData] = addPlayers(playerList.data.players);
+
+        const playerListString = await generatePlayerList(playerData);
+        const defenseListString = await generateDefenseList(teamData);
+        res.status(200).send({ playerListString, defenseListString })
+    } catch(e) {
+      console.log(`get-player-lists api error:  ${e}`);
+      res.status(500).send(e);
+    }
+}
+
+const addPlayers = (data) => {
+    const sortedData = [];
+    const teamData = [];
+    const teamIDs = [];
+    data.forEach(element => {
+        const team = element['player']['currentTeam'] === null ? 'NULL' : element['player']['currentTeam']['abbreviation'];
+        const teamID = element['player']['currentTeam'] === null ? 'NULL' : element['player']['currentTeam']['id'];
+        
+        sortedData.push([element['player']['id'], 
+                        element['player']['firstName']+' '+element['player']['lastName'],
+                        element['player']['primaryPosition'],
+                        team]);
+        if(!teamIDs.includes(teamID)){
+            teamIDs.push(teamID);
+            teamData.push([teamID,team]);
+        }
+
+    });
+    console.log(sortedData);
+    return [sortedData, teamData];
+}
+
+const generatePlayerList = async (playerData) => {
+    try{        
+        const SQLQueryString = `INSERT INTO public.player_list (player_id, player_name, position, nfl_team) VALUES %L returning player_id`;
+        let formatString = format(SQLQueryString, playerData);
+        console.log(formatString);        
+        await db.query(formatString);
+        return "SQL operation complete: " + formatString;
+    } catch(err){
+        console.log(`Error occured adding players in generatePlayerlist: ${err}`)
+    };
+};
+
+const generateDefenseList = async (teamData) => {
+    try{
+        const SQLQueryString = `INSERT INTO public.def_list (def_id, nfl_team) VALUES %L returning def_id`;
+        let formatString = format(SQLQueryString, teamData);
+        console.log(formatString);
+        await db.query(formatString);
+        return "SQL operation complete: " + formatString;
+    } catch(err) {
+        console.log(`Error occured adding players in generatePlayerlist: ${err}`);
+    }
+};
