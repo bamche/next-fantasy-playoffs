972a0a7e66a9e6087fb3a64b34e5169ec67a55d7
Blake <blake.myrick@gmail.com>
2026-01-14 01:21:31 -0800

    update week 2 gameconstants

--- constants/game/gameConstants.js
+++ constants/game/gameConstants.js
@@ -1,32 +1,52 @@
 export const gameConstants = [
+    // {
+    //     gameId: '401772979',
+    //     gameTimeStart: '10 Jan 2026 21:30:00 GMT',
+    //     gameTitle: 'rams-panthers'
+    // },
+    // {
+    //     gameId: '401772981',
+    //     gameTimeStart: '11 Jan 2026 01:00:00 GMT',
+    //     gameTitle: 'packers-bears'
+    // },
+    // {
+    //     gameId: '401772977',
+    //     gameTimeStart: '11 Jan 2026 18:00:00 GMT',
+    //     gameTitle: 'bills-jaguars'
+    // },
+    // {
+    //     gameId: '401772980',
+    //     gameTimeStart: '11 Jan 2026 21:30:00 GMT',
+    //     gameTitle: '49ers-eagles'
+    // },
+    // {
+    //     gameId: '401772978',
+    //     gameTimeStart: '12 Jan 2026 01:00:00 GMT',
+    //     gameTitle: 'chargers-patriots'
+    // },
+    // {
+    //     gameId: '401772976',
+    //     gameTimeStart: '13 Jan 2026 01:15:00 GMT',
+    //     gameTitle: 'texans-steelers'
+    // },
     {
-        gameId: '401772979',
-        gameTimeStart: '10 Jan 2026 21:30:00 GMT',
-        gameTitle: 'rams-panthers'
+        gameId: '401772982',
+        gameTimeStart: '17 Jan 2026 21:30:00 GMT',
+        gameTitle: 'bills-broncos'
     },
     {
-        gameId: '401772981',
-        gameTimeStart: '11 Jan 2026 01:00:00 GMT',
-        gameTitle: 'packers-bears'
+        gameId: '401772984',
+        gameTimeStart: '18 Jan 2026 01:00:00 GMT',
+        gameTitle: '49ers-seahawks'
     },
     {
-        gameId: '401772977',
-        gameTimeStart: '11 Jan 2026 18:00:00 GMT',
-        gameTitle: 'bills-jaguars'
+        gameId: '401772983',
+        gameTimeStart: '18 Jan 2026 20:00:00 GMT',
+        gameTitle: 'texans-patriots'
     },
     {
-        gameId: '401772980',
-        gameTimeStart: '11 Jan 2026 21:30:00 GMT',
-        gameTitle: '49ers-eagles'
-    },
-    {
-        gameId: '401772978',
-        gameTimeStart: '12 Jan 2026 01:00:00 GMT',
-        gameTitle: 'chargers-patriots'
-    },
-    {
-        gameId: '401772976',
-        gameTimeStart: '13 Jan 2026 01:15:00 GMT',
-        gameTitle: 'texans-steelers'
+        gameId: '401772985',
+        gameTimeStart: '18 Jan 2026 23:30:00 GMT',
+        gameTitle: 'rams-bears'
     }
 ]
